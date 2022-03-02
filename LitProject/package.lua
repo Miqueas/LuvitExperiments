b1d03f4ad39dec26fe5d5d67a76e9598ec3a0798
Miqueas <miqueas2020@yahoo.com>
2022-03-02 04:22:52 +0000

    Added lit example

---
+++ LitProject/package.lua
@@ -0,0 +1,11 @@
+return {
+  name = "LuvitExperiments",
+  version = "0.0.1",
+  luvi = { version = "2.13.0", flavor = "regular" },
+  dependencies = {},
+  description = "Lit example project",
+  tags = { "lua", "lit", "luvit", "example" },
+  license = "MIT",
+  author = { name = "Miqueas", email = "miqueas2020@yahoo.com" },
+  homepage = "https://github.com/Miqueas/LuvitExperiments"
+}
